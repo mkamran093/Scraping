1ed5f608b741fcb670b50e0e7736e51ee15f9b48
Muhammad Kamran <mkamran.bscssef20@iba-suk.edu.pk>
2024-09-06 20:54:51 +0500

    Initial requirements fulfilled

--- Odoo Scraper/igc_scraper.py
+++ Odoo Scraper/igc_scraper.py
@@ -2,7 +2,6 @@ import logging
 import psutil
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
